2baea2f7b2a8fe62d66e0a5afbb7d4ce950d9b5f
Rory Brookes <rorybrookes89@hotmail.com>
2016-10-29 15:40:31 +0900

    add exercise 3 4 5

---
+++ OOP/chapter_1/exercise_3.rb
@@ -0,0 +1,48 @@
+class MyCar
+  attr_accessor :color, :year, :model, :speed
+  
+  def initialize(color, year, model)
+    @color = color
+    @year = year
+    @model = model
+    @speed = 0
+  end
+  
+  def shop!(color, year, model)
+    if self.speed > 0
+      puts "Speed reduced to zero!"
+    end
+    self.color = color
+    self.year = year
+    self.model = model
+    self.speed = 0
+  end
+
+  def speed_up
+    self.speed+=1
+    p self.speed
+  end
+  
+  def slow_down
+    unless speed == 0
+      self.speed-=1
+    else
+      p "Can't go slower than zero!"
+    end
+  end
+end
+
+car = MyCar.new("red",1989, "Toyota")
+puts car.color
+puts car.year
+puts car.model
+
+car.shop!("blue","1989","Toyota")
+puts car.color
+3.times do
+  car.speed_up
+end
+
+4.times do
+  car.slow_down
+end
---
+++ OOP/chapter_1/exercise_5.rb
@@ -0,0 +1,48 @@
+class MyCar
+  attr_accessor :color, :speed
+  attr_reader :year, :model
+  
+  def initialize(color, year, model)
+    @color = color
+    @year = year
+    @model = model
+    @speed = 0
+  end
+  
+  def paint(color)
+    if self.speed > 0
+      puts "Speed reduced to zero! can't paint cars that are moving"
+    end
+    self.color = color
+    self.speed = 0
+  end
+
+  def speed_up
+    self.speed+=1
+    p self.speed
+  end
+  
+  def slow_down
+    unless speed == 0
+      self.speed-=1
+    else
+      p "Can't go slower than zero!"
+    end
+  end
+end
+
+car = MyCar.new("red",1989, "Toyota")
+puts car.color
+puts car.year
+puts car.model
+
+car.paint("blue")
+puts car.model
+puts car.color
+3.times do
+  car.speed_up
+end
+
+4.times do
+  car.slow_down
+end
